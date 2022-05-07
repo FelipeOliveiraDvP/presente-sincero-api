76274eb32b9bad1a687e4186fa0d7926ec7c6e3e
Felipe de Oliveira <felipe.dev2020@gmail.com>
2022-05-07 17:11:56 -0300

    Criação do footer do site

--- resources/js/App.vue
+++ resources/js/App.vue
@@ -2,16 +2,19 @@
   <div>
     <site-header />
     <router-view />
+    <public-footer />
   </div>
 </template>
 
 <script>
 import HeaderVue from "./components/_commons/Header.vue";
+import PublicFooterVue from "./components/_commons/PublicFooter.vue";
 
 export default {
   name: "App",
   components: {
     "site-header": HeaderVue,
+    "public-footer": PublicFooterVue,
   },
 };
 </script>
---
+++ resources/js/components/_commons/PublicFooter.vue
@@ -0,0 +1,50 @@
+<template>
+  <footer class="public-footer">
+    <b-container fluid>
+      <b-row>
+        <b-col md="6" lg="3">
+          <h3>Mapa do site</h3>
+          <router-link to="/termos-de-uso">Termos de uso</router-link>
+        </b-col>
+        <b-col md="6" lg="3">
+          <h3>Formas de pagamento</h3>
+          <div>
+            <img src="/img/pix.png" alt="Pague com PIX" />
+            <img src="/img/mercado-pago.png" alt="Mercado Pago" />
+          </div>
+        </b-col>
+        <b-col md="6" lg="3">
+          <h3>Segurança</h3>
+          <div class="d-flex flex-column justify-content-center">
+            <img src="/img/site-seguro.png" alt="Site seguro" />
+            <img src="/img/federal.png" alt="Loteria Federal" />
+          </div>
+        </b-col>
+        <b-col md="6" lg="3">
+          <h3>Redes sociais</h3>
+          <ul class="social-list list-unstyled d-flex w-100">
+            <li>
+              <a href="#" target="_blank"><i class="fab fa-instagram"></i></a>
+            </li>
+            <li>
+              <a href="#" target="_blank"><i class="fab fa-facebook"></i></a>
+            </li>
+            <li>
+              <a href="#" target="_blank"><i class="fab fa-whatsapp"></i></a>
+            </li>
+          </ul>
+        </b-col>
+      </b-row>
+    </b-container>
+    <p class="copyright bg-dark text-white text-right p-3">
+      2022 - Todos os direitos reservados
+    </p>
+  </footer>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
