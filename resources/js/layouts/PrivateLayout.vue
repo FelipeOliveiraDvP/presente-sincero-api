3fed6dcf7f4a99fc59299bcae01097ca9549c8bf
Felipe de Oliveira <felipe.dev2020@gmail.com>
2022-05-09 04:44:36 -0300

    Estrutura do layout do admin

--- resources/js/layouts/PrivateLayout.vue
+++ resources/js/layouts/PrivateLayout.vue
@@ -1,7 +1,50 @@
 <template>
-  <div>
-    <header>Private</header>
-    <slot></slot>
+  <div class="private-layout">
+    <header class="private-header">
+      <div class="logo">LOGO</div>
+
+      <div class="user-info">
+        <span>
+          <b-avatar
+            src="https://picsum.photos/600/300/"
+            size="2.5rem"
+          ></b-avatar>
+          <span>Usuário</span>
+        </span>
+        <b-button variant="outline-danger"
+          ><i class="fas fa-sign-out-alt"></i> Sair</b-button
+        >
+      </div>
+    </header>
+    <div class="private-wrapper">
+      <aside class="menu">
+        <nav>
+          <ul>
+            <li>
+              <i class="fas fa-trophy"></i> Sorteios
+              <ul>
+                <li>
+                  <router-link to="/admin/sorteios"> Ver todos </router-link>
+                </li>
+                <li>
+                  <router-link to="/admin/sorteios/novo-sorteio">
+                    Novo sorteio
+                  </router-link>
+                </li>
+              </ul>
+            </li>
+            <li>
+              <router-link to="/admin/clientes">
+                <i class="fas fa-users"></i> Clientes
+              </router-link>
+            </li>
+          </ul>
+        </nav>
+      </aside>
+      <main class="content">
+        <slot></slot>
+      </main>
+    </div>
   </div>
 </template>
 
