b1f17ee570023de24835f1e465b3d61a8c911c92
Felipe de Oliveira <felipe.dev2020@gmail.com>
2022-06-02 05:47:27 -0300

    Criadas as tabelas de contas bancárias

---
+++ app/Models/BankAccount.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class BankAccount extends Model
+{
+    use HasFactory, SoftDeletes;
+
+    /**
+     * The database table.
+     * 
+     * @var string
+     */
+    protected $table = 'bank_account';
+
+    /**
+     * The soft delete date column.
+     * 
+     * @var array
+     */
+    protected $dates = ['deleted_at'];
+
+    /**
+     * Enable timestamps.
+     * 
+     * @var boolean
+     */
+    protected $timestatmps = true;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'user_id',
+        'name',
+        'cc',
+        'agency',
+        'dv',
+        'key',
+        'main',
+        'type',
+    ];
+
+    /**
+     * Cast attributes into other types.
+     * 
+     * @var array<string, string>
+     */
+    protected $casts = [
+        'main' => 'boolean'
+    ];
+}
---
+++ database/migrations/2022_06_02_082732_create_bank_accounts_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBankAccountsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('bank_accounts', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('user_id');
+            $table->string('name');
+            $table->string('cc')->nullable();
+            $table->string('agency')->nullable();
+            $table->string('dv')->nullable();
+            $table->string('key')->nullable();
+            $table->boolean('main')->default(false);
+            $table->enum('type', ['PIX', 'BANK']);
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('bank_accounts');
+    }
+}
