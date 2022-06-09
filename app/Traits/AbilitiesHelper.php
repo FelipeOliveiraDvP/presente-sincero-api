269b7b438fd10b873d5ddf4cb854d7fbc33edf7e
Felipe de Oliveira Souza <felipe.oliveirac8@gmail.com>
2022-06-08 23:12:35 -0300

    Criada trait com as permissões dos usuários

---
+++ app/Traits/AbilitiesHelper.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Traits;
+
+trait AbilitiesHelper
+{
+  /**
+   * Returns the simple login customer token abilities.
+   * 
+   * @return array<string>
+   */
+  protected function simpleCustomerAbilities()
+  {
+    return [
+      'reserve.numbers',
+      'free.numbers',
+    ];
+  }
+
+  /**
+   * Returns the full customer token abilities.
+   * 
+   * @return array<string>
+   */
+  protected function fullCustomerAbilities()
+  {
+    return [
+      'reserve.numbers',
+      'free.numbers',
+      'view.profile',
+      'edit.profile',
+    ];
+  }
+
+  /**
+   * Returns the seller token abilities.
+   * 
+   * @return array<string>
+   */
+  protected function sellerAbilities()
+  {
+    return [
+      'reserve.numbers',
+      'free.numbers',
+      'view.profile',
+      'edit.profile',
+      'create.contests',
+      'edit.contests',
+      'manage.contests',
+      'list.bank_accounts',
+      'create.bank_accounts',
+      'edit.bank_accounts',
+      'remove.bank_accounts',
+    ];
+  }
+}
