f84f963c5472b21dc0aa6f8406c1bff31bb5f239
Felipe de Oliveira Souza <felipe.oliveirac8@gmail.com>
2022-06-22 00:21:17 -0300

    título da página das variáveis de ambiente

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -5,7 +5,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="{{ mix('css/app.css') }}">
-    <title>Presente Sincero</title>
+    <title>{{ env('APP_NAME') }}</title>
 </head>
 
 <body>
