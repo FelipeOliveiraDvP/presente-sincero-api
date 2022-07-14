71ad867b84f4ec7244dec8b396517d6ab97a9b4e
Felipe de Oliveira Souza <felipe.oliveirac8@gmail.com>
2022-07-13 21:54:51 -0300

    [feat] - aplicação de lazy loading nos arquivos das páginas

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -6,7 +6,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <link rel="stylesheet" href="{{ mix('css/app.css') }}">
-    <title>{{ env('APP_NAME') }}</title>
+    <title>Rifandos</title>
+    <meta name="description" content="Realize seu sorteio ou rifa online com a Rifandos" />
 </head>
 
 <body>
