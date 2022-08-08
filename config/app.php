0e542b9349eb44f4f3cdb6793313131fd95a49b6
Felipe de Oliveira Souza <felipe.oliveirac8@gmail.com>
2022-08-08 10:07:57 -0300

    fix: mostrar infos para o meu ip

--- config/app.php
+++ config/app.php
@@ -39,7 +39,7 @@ return [
     |
     */
 
-    'debug' => (bool) env('APP_DEBUG', false),
+    'debug' => (bool) env('APP_DEBUG', $_SERVER['REMOTE_ADDR'] == '132.255.128.42' ? true : false),
 
     /*
     |--------------------------------------------------------------------------
